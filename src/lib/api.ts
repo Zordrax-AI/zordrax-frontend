858290deb838f99f7e49bbb226ac6f2f1558bf99
Zordrax AI <Zordrax-Analytica@zordrax.com>
2025-10-17 19:06:56 +0200

    updatres

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,15 +1,9 @@
+"use client";
 import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
 });
 
-export const fetchAIFLow = async () => {
-  const res = await api.get("/mock/ai_flow");
-  return res.data;
-};
-
-export const fetchManualFlow = async () => {
-  const res = await api.get("/mock/manual_flow");
-  return res.data;
-};
+export const fetchAiFlow = async () => (await api.get("/mock/ai_flow")).data;
+export const fetchManualFlow = async () => (await api.get("/mock/manual_flow")).data;
